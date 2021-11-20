e8cbb8ca87f8c3175f9743c109329f1a67c8ed0c
elliot <dhaka.sonu.monu@gmail.com>
2021-11-20 18:40:41 +0530

    how to create data.gov.in api-key

---
+++ login.py
@@ -0,0 +1,10 @@
+'''
+**`creating api key on data-gov-in`**
+- register and login
+- go to my account section(logged In) and
+- just click on create API key; there you have it
+- this key can be used for any datasset
+'''
+
+api_key=''
+# in '' goes your api-key that you can get 
